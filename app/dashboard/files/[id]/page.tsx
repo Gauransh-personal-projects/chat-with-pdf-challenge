edb8aa7e5c25de8fcf50075015978073f3b79541
GauranshMathur <40708506+GauranshMathur@users.noreply.github.com>
2024-07-26 03:59:57 +0800

    chore: Add Chat component to ChatToFilePage

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Chat from "@/components/Chat";
 import PdfView from "@/components/PdfView";
 import { adminDb } from "@/firebaseAdmin";
 import { auth } from "@clerk/nextjs/server";
@@ -26,7 +27,7 @@ async function ChatToFilePage({
       {/* Right */}
       <div className="col-span-5 lg:cold-span-2 overflow-y-auto">
         {/* Chat */}
-        <Chat />
+        <Chat id={id} />
       </div>
 
       {/* Left */}
